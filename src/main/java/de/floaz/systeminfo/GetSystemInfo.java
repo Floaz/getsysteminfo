9ed566e086b69086c73d615e07486f64e72cca65
Philipp René Thomas <thomy@uzuzente.de>
2013-03-30 21:24:51 +0100

    License!

--- src/main/java/de/floaz/systeminfo/GetSystemInfo.java
+++ src/main/java/de/floaz/systeminfo/GetSystemInfo.java
@@ -1,3 +1,21 @@
+/**
+ * This file is part of GetSystemInfo.
+ *
+ * Copyright (C) 2013 Philipp Thomas <thomas@rfh-koeln.de>
+ *
+ * GetSystemInfo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GetSystemInfo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.floaz.systeminfo;
 
 import java.io.BufferedReader;
@@ -14,6 +32,12 @@ import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
 
+
+/**
+ * GetSystemInfo
+ * 
+ * @author thomy
+ */
 public class GetSystemInfo {
 
 	static final boolean FORCE_ROOT_USER = false;
